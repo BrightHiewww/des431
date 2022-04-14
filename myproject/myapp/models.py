585f46688bca805f2caa859801a18ab7cdfc7673
Thanakit Lerttomolsakul <6222780569@g.siit.tu.ac.th>
2022-04-15 06:51:44 +0700

    adding 3 tables

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -18,7 +18,6 @@ class Customer(models.Model):
     # "python3.8 manage.py makemigrations" is used to generate sql
     # add 'myapp' in settings.py in myproject
     # don't forget to save each change
-    # what
 
     def __str__(self):
         return "%s"%(self.user)
@@ -28,3 +27,25 @@ def update_profile_signal(sender, instance, created, **kwargs):
     if created:
         Customer.objects.create(user=instance)
         print('update_profile_signal: create a profile')
+
+#SKU (sugle Keeping Unit)
+class Item(models.Model):
+    UNIT_NAME_CHOICE = [ ("ea","each"), ("kg","kilogram"), ("g","gram") ]
+    title = models.CharField(max_length=100)
+    unit = models.CharField(max_length=5, choices=UNIT_NAME_CHOICE, default="ea")
+    unit_price = models.DecimalField(max_digits=5, decimal_places=2) #999.99
+    image=models.ImageField(upload_to='myimages') #from week05
+    description=models.CharField(max_length=100, null=True, blank=True)
+    def __str__(self):
+        return "%s"%(self.title)
+
+class OrderItem(models.Model):
+    item = models.ForeignKey( Item, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0)
+
+class Invoice(models.Model):
+    customer = models.ForeignKey( Customer, on_delete=models.CASCADE)
+    order_item = models.ForeignKey( OrderItem, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return "invoice: %s %s"%(self.customer, self.created_at)
