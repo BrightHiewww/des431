75dc9c97ba16e39f57089bdf109614a63cfe9d58
Thanakit Lerttomolsakul <6222780569@g.siit.tu.ac.th>
2022-04-15 07:47:52 +0700

    docker?

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.http import HttpResponse
 from myapp import views
 
 urlpatterns = [
